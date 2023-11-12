4bd3a0f504635f49e88b21e52cd9cf1b27e0a90e
typoirash <90721338+typoirash@users.noreply.github.com>
2023-11-12 19:34:13 +0300

    Add files via upload

---
+++ PP_3_1_4/PP_3_1_4/src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.kata.spring.boot_security.demo.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import ru.kata.spring.boot_security.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("select u from User u join fetch u.roles where u.name = (:name)")
+    Optional<User> findByName(@Param("name") String name);
+}
